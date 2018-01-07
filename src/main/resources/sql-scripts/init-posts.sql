3fd57e5df2b1f50a94caa98c7e591cef316a8a7e
Jan Šubelj <jan.subelj010@gmail.com>
2018-01-07 18:45:40 +0100

    ratu na postgres dt

---
+++ src/main/resources/sql-scripts/init-posts.sql
@@ -0,0 +1,8 @@
+INSERT INTO albums (album_id, album_name, artist_id) VALUES (1, 'Slovenska Krv', 1);
+INSERT INTO albums (album_id, album_name, artist_id) VALUES (2, 'Tragom Hromoga Vuka', 1);
+
+INSERT INTO albums (album_id, album_name, artist_id) VALUES (3, 'Love for Sale', 2);
+INSERT INTO albums (album_id, album_name, artist_id) VALUES (4, 'Nightflight to Venus', 2);
+
+INSERT INTO albums (album_id, album_name, artist_id) VALUES (5, 'Small Actions-Big Reactions', 3);
+INSERT INTO albums (album_id, album_name, artist_id) VALUES (6, 'Finish in Balance', 3);
