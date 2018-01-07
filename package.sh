829c39a500c8b329dab07cb62288e7a9c582f296
Jan Šubelj <jan.subelj010@gmail.com>
2018-01-07 20:19:07 +0100

    spremenil postse

--- package.sh
+++ package.sh
@@ -4,3 +4,5 @@ docker build -t cleptes/posts:0.02 .
 docker stop posts
 docker rm posts
 docker run -d --name posts -p 8091:8091 cleptes/posts:0.02
+
+#docker run -d --name postgres-posts -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=posts -p 5441:5432 postgres:latest
